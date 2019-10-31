8b1068588faf93826ff74b7b95e6526157a498c4
Carly Pecora <carlyannpecora@gmail.com>
2019-10-31 23:28:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+var everybodyAllDay = []
+
 function takeANumber (katzDeliLine, name) {
   katzDeliLine.push(name)
+  everybodyAllDay.push(name)
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
