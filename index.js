dde082d1e560be8dab157a9409d6e0c9d2af965c
Carly Pecora <carlyannpecora@gmail.com>
2019-10-31 23:23:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ function currentLine(katzDeliLine) {
     for (var i = 0; i < katzDeliLine.length; i++) {
       peopleInLine.push(`${i + 1}. ${katzDeliLine[i]}`)
     }
-    return `The line is currently: ${peopleInLine}
+    return `The line is currently: ${peopleInLine.join(", ")}`
   }
 }
