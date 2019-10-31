702e560d5b298da11ea05ffa77c4477e6f5d3bfb
Carly Pecora <carlyannpecora@gmail.com>
2019-10-31 23:14:42 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function takeANumber (katzDeliLine, name) {
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return 'There is nobody waiting to be served!'
+  } else {
+    var firstInLine = katzDeliLine[0]
+    katzDeliLine.shift(firstInLine)
+    return `Currently serving ${firstInLine}.`
+  }
+}
+
+function currentLine(katzDeliLine) {
+   if (katzDeliLine.length === 0) {
+    return 'The line is currently empty.'
+  } else {
+    let peopleInLine = [];
+    for (var i = 0; i < katzDeliLine.length; i++) {
+      peopleInLine.push(`${i + 1}. ${katzDeliLine[i]}`)
+    }
+  }
+}
